5b2f949cb5f5fc8ff3420f59eef00d0357e2c206
isimfr <onurlalsacien26@hotmail.fr>
2017-11-28 12:21:31 +0100

    add fction 3

--- main_test.py
+++ main_test.py
@@ -4,8 +4,12 @@ def fction1():
 def fction2():
     print("ajout fction 2")
 
+def fction3():
+    print("ajout fction 3")
+
 
 if __name__ == '__main__':
     fction1()
     fction2()
+    fction3()
 
